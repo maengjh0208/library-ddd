9cf36b3145014361772c9fd2998e973882bd7de4
maengjh <juhee.bda@gmail.com>
2026-05-20 00:20:49 +0900

    feat: Book Entity 추가

---
+++ domain/book.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from domain.isbn import ISBN
+
+
+@dataclass
+class Book:
+    isbn: ISBN  # ISBN
+    title: str  # 책 제목
+    author: str  # 저자
+    is_available: bool = True  # 대출 가능 여부. 기본값 True (처음엔 대출 가능으로 설정)
+
+    def __eq__(self, other):
+        if not isinstance(other, Book):
+            return False
+
+        return self.isbn == other.isbn
+
+    # __eq__를 직접 정의하면 Python이 __hash__를 None으로 만들어버린다.
+    def __hash__(self):
+        return hash(self.isbn)
