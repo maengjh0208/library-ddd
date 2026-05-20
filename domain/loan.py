be505607aedf1846bbf312130f5bc10c83ccc944
maengjh <juhee.bda@gmail.com>
2026-05-20 17:03:21 +0900

    feat: Loan Aggregate Root 추가

---
+++ domain/loan.py
@@ -0,0 +1,67 @@
+"""
+Loan Aggregate Root:
+ISBN(Value Object), Book(Entity, 책), Member(Entity, 회원)을
+하나로 묶어서 관리하는 것이 Aggregate Root이다.
+
+Loan (Aggregate Root)
+├── Book  (어떤 책을?)
+├── Member (누가?)
+└── 대출일, 반납기한, 반납여부 (언제?)
+
+왜 Loan이 Aggregate Root 인가?
+'대출'없이는 Book과 Member가 연결될 수 없다.
+Book.is_available, Member.loan_count 등은 누가 바꾸나?
+바로 Loan이 책임지고 관리한다.
+즉 외부에서 Book이나 Member를 직접 건드리지 않고,
+반드시 Loan을 통해서만 상태가 바뀌어야 한다.
+"""
+
+from dataclasses import dataclass, field
+from datetime import date, timedelta
+
+from domain.book import Book
+from domain.member import Member
+
+
+@dataclass
+class Loan:
+    loan_id: str  # 대출 식별자
+    book: Book  # Book Entity
+    member: Member  # Member Entity
+    loan_date: date  # 대출일
+    is_returned: bool = False  # 반납 여부 (기본값 False)
+    due_date: date = field(
+        init=False
+    )  # 반납기한 (대출일 + 14일). field(init=False) -> __init__ 파라미터에서 제외됨
+
+    # 책 반납
+    def return_book(self) -> None:
+        self.is_returned = True
+
+        # 책 반납 가능 상태로 변경
+        self.book.is_available = True
+
+        # 회원 대출 권수 감소
+        self.member.loan_count -= 1
+
+    # __init__이 실행된 직후 자동으로 호출되는 메서드
+    def __post_init__(self) -> None:
+        # 유효성 검사 - 책이 대출 불가능한 상태이거나 회원이 책을 대출할 수 있는 상태가 아니면 에러
+        if not self.book.is_available:
+            raise ValueError(
+                f"책 대출 불가 - 책이 이용가능한 상태가 아님 | book:{self.book} member:{self.member}"
+            )
+
+        if not self.member.can_borrow():
+            raise ValueError(
+                f"책 대출 불가 - 회원이 대출 가능한 상태가 아님 | book:{self.book} member:{self.member}"
+            )
+
+        # 책 대출
+        self.book.is_available = False
+        self.member.loan_count += 1
+        self.due_date = self.loan_date + timedelta(days=14)
+
+    # 연체 여부 조회
+    def is_overdue(self) -> bool:
+        return date.today() > self.due_date
