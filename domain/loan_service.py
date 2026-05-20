733d69c72c5d7cfbad317ef594a9ee38a6142176
maengjh <juhee.bda@gmail.com>
2026-05-20 17:22:11 +0900

    feat: LoanService domain service 추가

---
+++ domain/loan_service.py
@@ -0,0 +1,9 @@
+from domain.book import Book
+from domain.member import Member
+
+
+class LoanService:
+    def can_loan(self, book: Book, member: Member) -> bool:
+        # 조건 1 - 책이 대출 가능한 상태인가?
+        # 조건 2 - 회원이 대출 가능한 상태인가?
+        return book.is_available and member.can_borrow()
